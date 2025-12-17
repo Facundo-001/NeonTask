431f8544cc7ec14723ed07876ad5f883ee34cb4c
Facundo-001 <facundo2021xd@gmail.com>
2025-12-17 16:53:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'celionic',
+  webDir: 'dist'
+};
+
+export default config;
